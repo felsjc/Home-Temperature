61d7fe442ae1fcf098e7a85b645a0a50a423ec01
Felipe <felsjc@gmail.com>
2019-09-04 20:55:51 +0200

    Androidx Migration

--- app/src/main/java/com/example/myweatherdatabase/utilities/ParserUtils.java
+++ app/src/main/java/com/example/myweatherdatabase/utilities/ParserUtils.java
@@ -2,8 +2,8 @@ package com.example.myweatherdatabase.utilities;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.example.myweatherdatabase.data.AppPreferences;
